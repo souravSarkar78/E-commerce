23874c01ab6da9ef8629aa391bf516ef7bf3b3e3
souravSarkar78 <ss830897@gmail.com>
2022-02-07 23:38:24 +0530

    Test

--- Frontend/ecommerce/components/Homepage/Topbar.vue
+++ Frontend/ecommerce/components/Homepage/Topbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed top-0 h-14 md:h-20 left-0 md:left-60 right-0 text-center bg-green-300 duration-75 flex justify-center z-10">   <!-- //fixed sorale neme jacche body -->
-        <div class="inner-container w-full md:w-2/3 m-1 bg-blue-500 flex space-around items-center justify-around">
+        <div class="inner-container w-full md:w-10/12 m-1 bg-blue-500 flex space-around items-center justify-around">
         <div class="w-2/3">
         <p class="md:hidden" @click="OpenLeftNavbar()"><fa :icon="['fas','align-justify']" /></p>
         <p class="hidden md:block">Searchbar</p>
