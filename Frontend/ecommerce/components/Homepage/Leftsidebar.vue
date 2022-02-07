6dc0669b7229f881f5bdad23c6eacf8412054cd1
souravSarkar78 <ss830897@gmail.com>
2022-02-08 01:07:23 +0530

    fix: sidebar shifting issue

--- Frontend/ecommerce/components/Homepage/Leftsidebar.vue
+++ Frontend/ecommerce/components/Homepage/Leftsidebar.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="sidebar fixed top-0 h-screen p-2 overflow-y-auto text-center bg-red-300 z-20">
+    <div class="sidebar fixed top-0 h-screen p-2 overflow-y-auto text-center bg-yellow-600 z-20">
         <!-- <div v-for="i in 100" key:"i">{{i}}</div> -->
         <div @click="OpenLeftNavbar()" class="">
-          <p class="text-2xl relative -right-20 md:hidden">
+          <p class="text-2xl relative float-right right-4 md:hidden">
             <fa icon="window-close" />
           </p>
-            <div class="bg-red-500">
+            <div class="bg-red-500 relative top-10">
               <NuxtLink to="AllProducts">Products</NuxtLink>
             </div>
         </div>
-        {{this.$store.state.NavController.leftSidebarOpen}}
+        <!-- {{this.$store.state.NavController.leftSidebarOpen}} -->
     </div>
 </template>
 
--- Frontend/ecommerce/layouts/default.vue
+++ Frontend/ecommerce/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="overflow-hidden">
-    <Leftsidebar class="duration-200 w-60 md:left-0" v-bind:class="{ 'left-0' : this.$store.state.NavController.leftSidebarOpen, '-left-60' : !this.$store.state.NavController.leftSidebarOpen }"/>
-    <div class=" boder-box left-0 md:left-60 md:right-0 duration-200 w-full md:w-auto h-fit" v-bind:class="{ 'left-60' : this.$store.state.NavController.leftSidebarOpen }">
+    <Leftsidebar class="duration-200 w-80 md:w-60 md:left-0" v-bind:class="{ 'left-0' : this.$store.state.NavController.leftSidebarOpen, '-left-80' : !this.$store.state.NavController.leftSidebarOpen }"/>
+    <div class="boder-box left-0 md:left-60 md:right-0 duration-200 w-full md:w-auto h-fit" v-bind:class="{ 'left-60' : this.$store.state.NavController.leftSidebarOpen }">
       <Topbar class=""/>
       <Nuxt class="mt-14 md:mt-20 md:ml-60" />
       <!-- <Downbar class=""/> -->
